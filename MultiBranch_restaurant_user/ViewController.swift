deee69f4d1922569eba2c93cb4a1a07009cf663c
Aftab khan <bsse20872@gmail.com>
2025-10-01 22:42:32 +0100

    Initial Commit

---
+++ MultiBranch_restaurant_user/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiBranch_restaurant_user
+//
+//  Created by aftab khan on 01/10/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
